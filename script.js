87051cef2b13b585b8c99d00646a4ba2c9e976ea
Brian Cox <briancxx@icloud.com>
2020-02-04 11:51:27 -0600

    Homepage modifications for desktop interface

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function () {
     // On Load
-    $(".logo").height($(window).height() / 2);
+    $(".logo").height($(window).height() - $(".header").height());
 
     // Event Handlers
     $(window).resize(function () {
-        $(".logo").height($(window).height() / 2);
+        $(".logo").height($(window).height() - $(".header").height());
     });
 });
