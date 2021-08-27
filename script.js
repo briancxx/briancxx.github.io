14fd722ecd0faca946f7007ee80a2117d35bc9f2
Brian Cox <10470194+briancxx@users.noreply.github.com>
2021-08-27 16:31:30 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    // On Load
-    $(".logo").height($(window).height() - $(".header").height());
-
-    // Event Handlers
-    $(window).resize(function () {
-        $(".logo").height($(window).height() - $(".header").height());
-    });
-});
